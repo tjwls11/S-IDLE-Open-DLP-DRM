2b14bbb4fd32e1f19395da6827a5b6db76eaed99
tjwls <09sj03@naver.com>
2025-02-16 00:06:43 +0900

    api connection

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware  
 
 app = FastAPI()
+
+# CORS 설정 추가
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 모든 도메인에서 요청을 허용
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 HTTP 메서드 허용
+    allow_headers=["*"],  # 모든 헤더 허용
+)
 
 @app.get("/")
 def read_root():
