a54716c3e6b206f0bcd0f5aca81a829b8c742276
tjwls <09sj03@naver.com>
2025-02-22 23:29:19 +0900

    CORSMiddleware import error

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from routers import files, policy, access, encrypt, logs
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="DLP/DRM API")
 
@@ -8,6 +9,15 @@ app.include_router(policy.router, prefix="/policy", tags=["Policy"])
 app.include_router(access.router, prefix="/access", tags=["Access Control"])
 app.include_router(encrypt.router, prefix="/encrypt", tags=["Encryption"])
 app.include_router(logs.router, prefix="/logs", tags=["Logs"])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://34.41.82.222:3000"],  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
+
 
 @app.get("/")
 def root():
